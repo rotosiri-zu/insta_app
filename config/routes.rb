bdb0197674e84727d1edc45181e2f1520d2b7830
Yusuke Nakano <kaiseryusuke0509@gmail.com>
2019-03-09 09:36:44 +0000

    create user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
   root 'static_pages#home'
+  resources :users, only: %i(show)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_08_122942) do
+ActiveRecord::Schema.define(version: 2019_03_09_092336) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_03_08_122942) do
     t.text "bio"
     t.string "phone_number"
     t.string "sex"
+    t.string "profile_photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
