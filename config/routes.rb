6b5bc40d4f76b11ed7016df324506ed76ed9b9bb
Yusuke Nakano <kaiseryusuke0509@gmail.com>
2019-03-11 07:11:22 +0000

    add post show and destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
   root 'static_pages#home'
   resources :users, only: %i(show)
-  resources :posts, only: %i(index new create) do
+  resources :posts, only: %i(index new create show destroy) do
     resources :photos, only: %i(create)
   end
 end
