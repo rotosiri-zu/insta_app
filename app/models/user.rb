07bd26c5632ce344e4067c65a5e04b649488d21c
Yusuke Nakano <kaiseryusuke0509@gmail.com>
2019-03-11 07:53:57 +0000

    add static infomation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :posts, dependent: :destroy
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
   has_many :passive_relationships, class_name:  "Relationship",
                                    foreign_key: "followed_id",
                                    dependent:   :destroy
