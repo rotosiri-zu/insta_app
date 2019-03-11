e20c73071cb260a792fd3c8460ebd06733256e26
Yusuke Nakano <kaiseryusuke0509@gmail.com>
2019-03-11 05:37:54 +0000

    create post mechanism

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ gem 'omniauth-facebook'
 gem 'dotenv-rails'
 gem 'carrierwave',             '1.2.2'
 gem 'mini_magick',             '4.7.0'
+gem 'font-awesome-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
   root 'static_pages#home'
   resources :users, only: %i(show)
+  resources :posts, only: %i(index new create) do
+    resources :photos, only: %i(create)
+  end
 end
