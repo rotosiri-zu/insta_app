ce6faa42880e45979ff320d19f912b290b4efeb4
Yusuke Nakano <kaiseryusuke0509@gmail.com>
2019-03-14 03:05:11 +0000

    add title-name for every pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,13 @@ module ApplicationHelper
   def current_user?(user)
     user == current_user
   end
+  
+  def full_title(page_title = '')                     
+    base_title = "Insta_App"  
+    if page_title.empty?                              
+      base_title                                      
+    else 
+      page_title + " | " + base_title                 
+    end
+  end
 end
