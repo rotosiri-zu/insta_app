ac2e0e0bb1e28e4b7689cc4ec2e5ffad294e3519
kaiser-yu <kaiseryusuke0509@gmail.com>
2019-05-08 18:39:16 +0900

    setup rspec

--- config/application.rb
+++ config/application.rb
@@ -10,12 +10,20 @@ module InstaApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    
+
     config.i18n.default_locale = :ja
     config.action_view.embed_authenticity_token_in_remote_forms = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
   end
 end
