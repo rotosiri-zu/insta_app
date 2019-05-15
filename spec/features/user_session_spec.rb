1eaf41ea822e748054fc30179bf2ecc6eee5d248
kaiser-yu <kaiseryusuke0509@gmail.com>
2019-05-16 00:56:23 +0900

    add user's edit spec

--- spec/features/user_session_spec.rb
+++ spec/features/user_session_spec.rb
@@ -12,4 +12,21 @@ RSpec.feature "UserSessions", type: :feature do
     click_button "登録する"
     expect(page).to have_content "アカウント登録を受け付けました。"
   end
+
+  scenario "edit user's profile" do
+    user = FactoryBot.create(:user)
+    sign_in user
+    visit edit_user_registration_path
+    fill_in "ウェブサイト", with: "ウェブサイト"
+    fill_in "自己紹介" , with: "自己紹介"
+    fill_in "電話番号", with: "000-0000-0000"
+    select  "男性", from: "性別"
+    fill_in "現在のパスワード", with: user.password
+    click_button "変更する"
+    expect(page).to have_content "アカウントが更新されました。"
+    expect(user.reload.website).to eq "ウェブサイト"
+    expect(user.reload.bio).to eq "自己紹介"
+    expect(user.reload.phone_number).to eq "000-0000-0000"
+    expect(user.reload.sex).to eq "男性"
+  end
 end
