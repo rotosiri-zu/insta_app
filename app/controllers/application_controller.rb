8e7f6f3c58629b6345e03e9481e5a42a535c7c98
kaiser-yu <kaiseryusuke0509@gmail.com>
2019-05-30 20:10:41 +0900

    add application_controller notification count method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :get_notification_count
+
+  def get_notification_count
+    @notification_count = Notification.where(to_user_id:current_user.id, checked: false).count
+  end
 
   protected
 
