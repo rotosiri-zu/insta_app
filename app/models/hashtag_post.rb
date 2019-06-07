d43504cc722f6e54f8d602b432a9022fa241319e
kaiser-yu <kaiseryusuke0509@gmail.com>
2019-06-07 14:35:48 +0900

    make association between Post and Hashtag

--- app/models/hashtag_post.rb
+++ app/models/hashtag_post.rb
@@ -1,2 +1,6 @@
 class PostHashtag < ApplicationRecord
+  belongs_to :post
+  belongs_to :hashtag
+  validates  :micropost_id, presence: true
+  validates  :Hashtag_id,   presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,8 @@ class Post < ApplicationRecord
   has_many :photos, dependent: :destroy
   has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
   has_many :comments, -> { order(created_at: :desc) }, dependent: :destroy
-  
+  has_and_belongs_to_many :hashtags
+
   def liked_by(user)
     Like.find_by(user_id: user.id, post_id: id)
   end
