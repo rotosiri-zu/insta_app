283a95194d1225e8a593a62e70e2cc8a0c5b3f26
kaiser-yu <kaiseryusuke0509@gmail.com>
2019-06-07 18:25:53 +0900

    modify slice method

--- app/models/hashtag_post.rb
+++ app/models/hashtag_post.rb
@@ -1,6 +1,6 @@
 class PostHashtag < ApplicationRecord
   belongs_to :post
   belongs_to :hashtag
-  validates  :micropost_id, presence: true
+  validates  :post_id, presence: true
   validates  :Hashtag_id,   presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,14 +3,15 @@ class Post < ApplicationRecord
   has_many :photos, dependent: :destroy
   has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
   has_many :comments, -> { order(created_at: :desc) }, dependent: :destroy
-  has_and_belongs_to_many :hashtags
+  has_many :hashtag_posts
+  has_many :hashtags, through: :hashtag_posts
 
   after_create do
     post = Post.find_by(id: self.id)
     # 半角もしくは全角の"#"に続けて、#やスペース以外の文字が１文字以上続く文字列をハッシュタグとする
     hashtags  = self.caption.scan(/[#＃][^#＃\p{blank}]+/)
     hashtags.uniq.map do |hashtag|
-      tag = Hashtag.find_or_create_by(hashname: hashtag.slice(0)) #先頭の"#"は除いて保存
+      tag = Hashtag.find_or_create_by(hashname: hashtag.slice(1..-1)) #先頭の"#"は除いて保存
       post.hashtags << tag
     end
   end
