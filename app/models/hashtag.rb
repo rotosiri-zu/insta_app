217e86d7ca07390f712a2c71fb0a0cbb23773c8c
kaiser-yu <kaiseryusuke0509@gmail.com>
2019-06-07 21:43:53 +0900

    add method for count users

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -2,4 +2,8 @@ class Hashtag < ApplicationRecord
   has_many :hashtag_posts
   has_many :posts, through: :hashtag_posts
   validates :hashname, presence: true, length: {maximum:50}
+
+  def posted_users
+    self.posts.map{|post| post.user}.uniq
+  end
 end
