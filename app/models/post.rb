c567f5f136ca3012d3e04f6d62070cf57688875f
kaiser-yu <kaiseryusuke0509@gmail.com>
2019-06-07 15:46:05 +0900

    post作成後のhashtagの作成設定
    postのキャプションから#から始まる文字列を取得し、ハッシュタグを生成するように設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,16 @@ class Post < ApplicationRecord
   has_many :comments, -> { order(created_at: :desc) }, dependent: :destroy
   has_and_belongs_to_many :hashtags
 
+  after_create do
+    post = Post.find_by(id: self.id)
+    # 半角もしくは全角の"#"に続けて、#やスペース以外の文字が１文字以上続く文字列をハッシュタグとする
+    hashtags  = self.caption.scan(/[#＃][^#＃\p{blank}]+/)
+    hashtags.uniq.map do |hashtag|
+      tag = Hashtag.find_or_create_by(hashname: hashtag.slice(0)) #先頭の"#"は除いて保存
+      post.hashtags << tag
+    end
+  end
+
   def liked_by(user)
     Like.find_by(user_id: user.id, post_id: id)
   end
