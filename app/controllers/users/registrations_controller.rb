fadd9fa7cafd2e10d59e1a4e891b842c345db14e
kaiser-yu <kaiseryusuke0509@gmail.com>
2019-06-09 13:10:10 +0900

    refactor user edit page's layout and update without_current_password

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+
+  protected
+
+  def update_resource(resource, params)
+    return super if params["password"]
+    resource.update_without_password(params.except("current_password"))
+  end
+
+end
