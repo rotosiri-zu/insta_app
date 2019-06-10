f1fcbf23958def8451e72458e18a91582f77364f
kaiser-yu <kaiseryusuke0509@gmail.com>
2019-06-10 12:30:56 +0900

    modify DM ajax and remove pusher

--- app/controllers/direct_message_spaces_controller.rb
+++ app/controllers/direct_message_spaces_controller.rb
@@ -1,25 +1,25 @@
 class DirectMessageSpacesController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
     current_user_dm_spaces = DirectMessageSpaceUser.where(user_id: current_user.id).map(&:direct_message_space)
-    dm_space = DirectMessageSpaceUser.where(direct_message_space: current_user_dm_spaces, user_id: params[:user_id]).
+    @dm_space = DirectMessageSpaceUser.where(direct_message_space: current_user_dm_spaces, user_id: params[:user_id]).
       map(&:direct_message_space).first
-    if dm_space.blank?
-      dm_space = DirectMessageSpace.create
-      DirectMessageSpaceUser.create(direct_message_space: dm_space, user_id: current_user.id)
-      DirectMessageSpaceUser.create(direct_message_space: dm_space, user_id: params[:user_id])
+    if @dm_space.blank?
+      @dm_space = DirectMessageSpace.create
+      DirectMessageSpaceUser.create(direct_message_space: @dm_space, user_id: current_user.id)
+      DirectMessageSpaceUser.create(direct_message_space: @dm_space, user_id: params[:user_id])
     end
-    redirect_to action: :show, id: dm_space.id
+    redirect_to action: :show, id: @dm_space.id
   end
 
   def show
-    dm_space = DirectMessageSpace.find_by(id: params[:id])
-    @dm_space_user = dm_space.direct_message_space_users.
+    @dm_space = DirectMessageSpace.find_by(id: params[:id])
+    @dm_space_user = @dm_space.direct_message_space_users.
       where.not(user_id: current_user.id).first.user
     @direct_message = current_user.direct_messages.build
     @direct_message.build_notification
-    @direct_messages = DirectMessage.includes(:user).where(direct_message_space: dm_space).order(:created_at)
+    @direct_messages = DirectMessage.includes(:user).where(direct_message_space: @dm_space).order(:created_at)
     @direct_messages.where.not(user_id: current_user.id).update_all(checked: true)
   end
 end
