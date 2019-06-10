4f53ac196eb90f9cd2e3b7edd45ec22283e0bb7c
kaiser-yu <kaiseryusuke0509@gmail.com>
2019-06-11 01:23:55 +0900

    DMSpaceUserモデルとユーザーの関連付けを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships,  source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   has_many :direct_messages, dependent: :destroy
+  has_many :direct_message_space_users, dependent: :destroy 
   has_many :notifications ,class_name: "Notification", foreign_key: "to_user_id", dependent: :destroy
   has_many :notifications ,class_name: "Notification", foreign_key: "from_user_id", dependent: :destroy
   has_many :users
